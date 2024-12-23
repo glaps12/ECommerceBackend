24c759919279c9a55504c2822911ee09eebb9df2
glaps12 <zlfgzl@hotmail.com>
2024-12-23 15:50:59 +0300

    Initial Commit...

---
+++ backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/glaps12/ecommerce/entity/Product.java
@@ -0,0 +1,63 @@
+package com.glaps12.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+@Entity
+@Data
+@Table(name = "product")
+
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id" , nullable = false)
+    private ProductCategory category;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @UpdateTimestamp
+    @Column(name = "last_updated")
+    private Date lastUpdated;
+
+
+
+
+
+
+
+
+}
